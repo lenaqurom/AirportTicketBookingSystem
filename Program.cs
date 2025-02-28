ed4003cd28a0ea794acc0e6561f7b4975f761512
Lina Qurom <you@example.com>
2025-02-28 17:57:37 +0200

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AirportTicketBookingSystem
+{
+    class AirportTicketBookingSystemMain
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
