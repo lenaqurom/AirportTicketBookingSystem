d2ca79357e70645db8ba0c3b18ea2b4616534132
Lina Qurom <you@example.com>
2025-03-03 01:18:23 +0200

    Add Validation Data

--- Services/FlightService.cs
+++ Services/FlightService.cs
@@ -15,6 +15,35 @@ namespace AirportTicketBookingSystem.Services
         {
             _flightRepository = new FlightRepository();
         }
+        public List<string> ValidateFlightData(Flight flight, List<string> existingFlightIds)
+        {
+            List<string> errors = new List<string>();
+            if (string.IsNullOrWhiteSpace(flight.FlightId))
+                errors.Add("Flight ID is required.");
+            else if(existingFlightIds.Contains(flight.FlightId))
+                errors.Add($"Duplicate Flight ID detected: {flight.FlightId}");
+
+            if(string.IsNullOrWhiteSpace(flight.DepartureCountry))
+                errors.Add("Departure Country is required.");
+            if (string.IsNullOrWhiteSpace(flight.DestinationCountry))
+                errors.Add("Destination Country is required.");
+
+            if (string.IsNullOrWhiteSpace(flight.DepartureAirport) || flight.DepartureAirport.Length != 3)
+                errors.Add("Departure Airport must be a 3-letter code.");
+            if (string.IsNullOrWhiteSpace(flight.ArrivalAirport) || flight.ArrivalAirport.Length != 3)
+                errors.Add("Arrival Airport must be a 3-letter code.");
+
+            if (flight.DepartureDate < DateTime.UtcNow.Date)
+                errors.Add($"Departure date {flight.DepartureDate.ToShortDateString()} is invalid. It must be today or a future date.");
+
+            foreach (var kvp in flight.TicketPrices)
+            {
+                if (kvp.Value <= 0)
+                    errors.Add($"Invalid price for {kvp.Key} class. Price must be greater than zero.");
+            }
+            
+            return errors;
+        }
         public List<Flight> SearchFlights(string? departureCountry, string? destinationCountry, decimal? maxPrice, DateTime? departureDate, string? departureAirport, string? arrivalAirport, string? flightClass)
         {
             var flights = _flightRepository.GetAllFlights();
